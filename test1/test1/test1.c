e4f57e4548377af56a9e09363541deae19f211a3
LandmineFly <2353789859@qq.com>
2023-03-07 21:11:25 +0800

    update

--- test1/test1/test1.c
+++ test1/test1/test1.c
@@ -1,5 +1,40 @@
 #include <stdio.h>
+#include <stdlib.h>
+#define MAX_SIZE 100
+
+struct List {
+	int* value;
+	int length;
+};
+
+void deleteAll(struct List* list, int item) {
+	int* left = list->value;
+	int* right = (list->value) + list->length - 1;
+	while (left < right) {
+		if (*left != item) {
+			left++;
+		} else {
+			*left = *right;
+			left++;
+			right--;
+			list->length--;
+		}
+	}
+}
 
 int main() {
-	printf("hello world!");
+	struct List list = { (int*) malloc(MAX_SIZE * sizeof(int)), 5 };
+	list.value[0] = 1;
+	list.value[1] = 2;
+	list.value[2] = 1;
+	list.value[3] = 5;
+	list.value[4] = 6;
+	for (int i = 0; i < list.length; i++) {
+		printf("%d ", list.value[i]);
+	}
+	printf("\n");
+	deleteAll(&list, 1);
+	for (int i = 0; i < list.length; i++) {
+		printf("%d ", list.value[i]);
+	}
 }
